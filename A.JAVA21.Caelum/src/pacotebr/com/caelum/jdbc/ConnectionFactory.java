ee9cbe1a9dc064706679bfbf4eca8c5c58f60948
jhondonavan <jhondonavan@gmail.com>
2016-02-23 10:54:41 -0300

    Correção na classe ConnectionFactory e criação de classe de teste para conexão

--- A.JAVA21.Caelum/src/pacotebr/com/caelum/jdbc/ConnectionFactory.java
+++ A.JAVA21.Caelum/src/pacotebr/com/caelum/jdbc/ConnectionFactory.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 //Metodo  getConnection para retornar uma nova conexão
-public class ConnectionFactor {
+public class ConnectionFactory {
 	
 	public Connection getConnection(){
 		try{
 			return  DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/TesteJDBC","postgres", "postgres");
+					"jdbc:postgresql://localhost:5432/A.JAVA21.Caelum","postgres", "postgres");
 		}catch (SQLException e){
 			throw new RuntimeException(e);
 		}
