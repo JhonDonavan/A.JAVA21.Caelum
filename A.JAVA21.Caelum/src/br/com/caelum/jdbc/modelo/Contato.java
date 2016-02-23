d9bca09c8ed2dd9a9021cdcfae0e8044445e945e
jhondonavan <jhondonavan@gmail.com>
2016-02-23 11:23:08 -0300

    Criação da classe Contato

---
+++ A.JAVA21.Caelum/src/br/com/caelum/jdbc/modelo/Contato.java
@@ -0,0 +1,44 @@
+package br.com.caelum.jdbc.modelo;
+
+import java.util.Calendar;
+
+public class Contato {
+	private long id;
+	private String nome;
+	private String email;
+	private String endereoco;
+	private Calendar dataNascimento;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereoco() {
+		return endereoco;
+	}
+	public void setEndereoco(String endereoco) {
+		this.endereoco = endereoco;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+
+}
