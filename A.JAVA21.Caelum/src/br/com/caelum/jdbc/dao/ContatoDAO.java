ce83ad614b9726d6f71c77bca6a3f1248ffaea61
jhondonavan <jhondonavan@gmail.com>
2016-02-23 13:57:24 -0300

    Metodo getList da classe contatoDAO concluida

--- A.JAVA21.Caelum/src/br/com/caelum/jdbc/dao/ContatoDAO.java
+++ A.JAVA21.Caelum/src/br/com/caelum/jdbc/dao/ContatoDAO.java
@@ -1,9 +1,13 @@
 package br.com.caelum.jdbc.dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.caelum.Connectionjdbc.ConnectionFactory;
 import br.com.caelum.jdbc.modelo.Contato;
@@ -35,4 +39,37 @@ public class ContatoDAO {
 				throw new RuntimeException(e);
 			}
 		}
+		
+		public List<Contato> getLista(){
+			try{
+				List<Contato> contatos = new ArrayList<Contato>();
+				PreparedStatement stmt = this.connection.prepareStatement("Select * from contatos");
+				ResultSet rs = stmt.executeQuery();
+				
+			while (rs.next()){
+				//Criando objeto contato
+				Contato contato  = new Contato();
+				contato.setId(rs.getLong("id"));
+				contato.setNome(rs.getString("Nome"));
+				contato.setEmail(rs.getString("Email"));
+				contato.setEndereoco(rs.getString("endereco"));
+				
+				//mostrar Data atraves do Calendar
+				Calendar data = Calendar.getInstance(); 
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+				
+				// adicionar objetos na lista
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+			} catch (SQLException e){
+				throw new RuntimeException(e);
+			}
+			
+		}
+		
+	
 }
