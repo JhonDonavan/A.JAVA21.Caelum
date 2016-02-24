49b5adcc6e3c10b099fe98305e88e3b957b4b9de
jhondonavan <jhondonavan@gmail.com>
2016-02-24 16:27:25 -0300

    Atualização no formato de datas

--- A.JAVA21.Caelum/src/br/com/caelum/jdbc/teste/TestaLista.java
+++ A.JAVA21.Caelum/src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -1,5 +1,9 @@
 package br.com.caelum.jdbc.teste;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import br.com.caelum.jdbc.dao.ContatoDAO;
@@ -13,12 +17,28 @@ public class TestaLista {
 		ContatoDAO dao = new ContatoDAO();
 		List<Contato> contatos = dao.getLista();
 		
+		/*String nascimento;
+		
+		SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+		sfd.format(contato.getDataNascimento().getTime())
+		Date data;
+		try{
+			data = format.parse(nascimento);
+		}catch(ParseException e){
+				data = null;
+			}*/
+		
 		for (Contato  contato  : contatos){
 			System.out.println("Nome: " + contato.getNome());
 			System.out.println("Email: " + contato.getEmail()); 
 			System.out.println("Endereço: "+ contato.getEndereoco());
-			System.out.println("Data de nascimento: " + contato.getDataNascimento().getTime() + "\n");
+			SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+			String dataformatada = sfd.format(contato.getDataNascimento().getTime());
+			System.out.println("Data de nascimento: " + dataformatada + "\n");
+			}  
+			
+			
 		}
 	}
 
-}
+
