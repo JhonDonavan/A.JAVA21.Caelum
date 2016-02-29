5797f78a3140d07f8bb648d4af34ca25095da13c
jhondonavan <jhondonavan@gmail.com>
2016-02-29 10:46:52 -0300

    Criado classe funcoinario e FuncionarioDAO(metodo inserir ok)

---
+++ A.JAVA21.Caelum/src/br/com/caelum/jdbc/dao/FuncionarioDAO.java
@@ -0,0 +1,35 @@
+package br.com.caelum.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.caelum.Connectionjdbc.ConnectionFactory;
+import br.com.caelum.jdbc.modelo.Funcionario;
+
+public class FuncionarioDAO {
+
+		private Connection connection;
+		
+		public FuncionarioDAO(){
+			this.connection = new ConnectionFactory().getConnection();
+		}
+		
+		public void adicionaFuncionario(Funcionario funcionario){
+			String sql = "insert into funcionarios " +
+							"(nome, usuario, senha)" +
+							"values(?,?,?)";
+			//preparar inserção no banco
+			
+			try{
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setString(1,funcionario.getNome());
+				stmt.setString(2, funcionario.getUsuario());
+				stmt.setString(3, funcionario.getSenha());
+				stmt.execute();
+				stmt.close();
+			} catch(SQLException e){
+				throw new RuntimeException(e); 
+			}
+		}
+}
